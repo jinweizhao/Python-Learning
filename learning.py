450835fc26819aaab388f174af353ff5bbbe73be
jinweizhao <694951521@qq.com>
2018-01-09 17:57:13 +0800

    高级特性完结

--- learning.py
+++ learning.py
@@ -390,41 +390,121 @@
 
 
 #杨辉三角
-def triangles():
-	b = [1]
-	while True:
-		yield b
-		temp = b[:]
-		temp.append(0)
-		temp = [temp[x - 1] + temp[x] for x in range(len(temp))]
-		b = temp[:]
+# def triangles():
+# 	b = [1]
+# 	while True:
+# 		yield b
+# 		temp = b[:]
+# 		temp.append(0)
+# 		temp = [temp[x - 1] + temp[x] for x in range(len(temp))]
+# 		b = temp[:]
 
-n = 0
-results = []
-for t in triangles():
-    print(t)
-    results.append(t)
-    # print(results)
-    n = n + 1
-    if n == 10:
-        break
+# n = 0
+# results = []
+# for t in triangles():
+#     print(t)
+#     results.append(t)
+#     # print(results)
+#     n = n + 1
+#     if n == 10:
+#         break
 
-if results == [
-    [1],
-    [1, 1],
-    [1, 2, 1],
-    [1, 3, 3, 1],
-    [1, 4, 6, 4, 1],
-    [1, 5, 10, 10, 5, 1],
-    [1, 6, 15, 20, 15, 6, 1],
-    [1, 7, 21, 35, 35, 21, 7, 1],
-    [1, 8, 28, 56, 70, 56, 28, 8, 1],
-    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
-]:
-    print('测试通过!')
-else:
-    print('测试失败!')
+# if results == [
+#     [1],
+#     [1, 1],
+#     [1, 2, 1],
+#     [1, 3, 3, 1],
+#     [1, 4, 6, 4, 1],
+#     [1, 5, 10, 10, 5, 1],
+#     [1, 6, 15, 20, 15, 6, 1],
+#     [1, 7, 21, 35, 35, 21, 7, 1],
+#     [1, 8, 28, 56, 70, 56, 28, 8, 1],
+#     [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+# ]:
+#     print('测试通过!')
+# else:
+#     print('测试失败!')
 	
+#迭代器
+#使用isinstance()判断一个对象是否是Iterable对象
+from collections import Iterable
+isinstance((x for x in range(10)), Iterable)
+#而生成器不但可以作用于for循环，还可以被next()函数不断调用并返回
+#   下一个值，直到最后抛出StopIteration错误表示无法继续返回下一个值了。
+
+# 可以被next()函数调用并不断返回下一个值的对象称为迭代器：Iterator。
+
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+# 把list、dict、str等Iterable变成Iterator可以使用iter()函数：
+
+isinstance(iter('abc'), Iterator)  # True
+
+# 你可能会问，为什么list、dict、str等数据类型不是Iterator？
+
+# 这是因为Python的Iterator对象表示的是一个数据流，Iterator对象可以被next()函数调用并不断返回下一个数据，
+# 直到没有数据时抛出StopIteration错误。可以把这个数据流看做是一个有序序列，但我们却不能提前知道序列的长度，
+# 只能不断通过next()函数实现按需计算下一个数据，所以Iterator的计算是惰性的，只有在需要返回下一个数据时它才会计算。
+
+# Iterator甚至可以表示一个无限大的数据流，例如全体自然数。而使用list是永远不可能存储全体自然数的
+
+
+# 凡是可作用于for循环的对象都是Iterable类型；
+
+# 凡是可作用于next()函数的对象都是Iterator类型，它们表示一个惰性计算的序列；
+
+# 集合数据类型如list、dict、str等是Iterable但不是Iterator，不过可以通过iter()函数获得一个Iterator对象。
+
+# Python的for循环本质上就是通过不断调用next()函数实现的，例如：
+
+# for x in [1, 2, 3, 4, 5]:
+#     pass
+# 实际上完全等价于：
+
+# 首先获得Iterator对象:
+# it = iter([1, 2, 3, 4, 5])
+# # 循环:
+# while True:
+#     try:
+#         # 获得下一个值:
+#         x = next(it)
+#     except StopIteration:
+#         # 遇到StopIteration就退出循环
+#         break
+
+
+#        7 函数式编程
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
