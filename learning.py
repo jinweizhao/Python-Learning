645224e7917b5b788f715e0df409ed3eeaac2e18
jinweizhao <694951521@qq.com>
2018-01-09 17:35:29 +0800

    高级特性完结

--- learning.py
+++ learning.py
@@ -48,7 +48,7 @@
 
 # def my_abs(x):
 #     if not isinstance(x, (int, float)):
-#         raise TypeError('bad operand type')
+#         raise TypeError('bad operand type') # 返回错误
 #     if x >= 0:
 #         return x
 #     else:
@@ -313,10 +313,118 @@
 
 
 #Python内置的enumerate函数可以把一个list变成索引-元素对
-for i, value in enumerate(['a', 'b', 'c']):
-	print(i, value)
+# for i, value in enumerate(['a', 'b', 'c']):
+# 	print(i, value)
 
 
+# 列表生成式
+# list(range(1, 5)) #[1, 2, 3, 4, 5]
+
+# [x * x for x in range(1, 11)] #[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+#for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方
+# [x * x for x in range(1, 11) if x % 2 == 0] #[4, 16, 36, 64, 100]
+
+#还可以使用两层循环，可以生成全排列
+# [m + n for m in 'ABC' for n in 'XYZ']
+#['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+
+#列表生成式也可以使用两个变量来生成list
+# d = {'x': 'A', 'y': 'B', 'z': 'C' }
+# [k + '=' + v for k, v in d.items()]   #['y=B', 'x=A', 'z=C']
+
+
+#生成器: 在Python中，这种一边循环一边计算的机制，称为生成器：generator，
+#第一种方法很简单，只要把一个列表生成式的[]改成()，就创建了一个generator
+# L = [x * x for x in range(10)]
+# g = (x * x for x in range(10))
+# print(L)
+# print(g)
+
+#斐波拉契数列（Fibonacci），除第一个和第二个数外，任意一个数都可由前两个数相加得到
+# def fib(max):
+# 	n ,a, b = 0, 0, 1
+# 	while n < max:
+# 		print(b)
+# 		a, b = b, a + b
+# 		n = n + 1
+# 	return 'done'
+# fib(6)
+
+#定义generator的另一种方法。如果一个函数定义中包含yield关键字，
+#那么这个函数就不再是一个普通函数，而是一个generator
+# def fib(max):
+# 	n ,a, b = 0, 0, 1
+# 	while n < max:
+# 		yield b
+# 		a, b = b, a + b
+# 		n = n + 1
+# 	return 'done'
+# f = fib(6)
+# print(f)
+
+#用for循环调用generator时，发现拿不到generator的return语句的返回值。
+#   如果想要拿到返回值，必须捕获StopIteration错误，返回值包含在StopIteration的value中
+# while True:
+# 	try:
+# 		x = next(f)
+# 		print(x)
+# 	except StopIteration as e:
+# 		print('Generator return value: ', e.value)
+# 		break
+
+
+#函数是顺序执行，遇到return语句或者最后一行函数语句就返回。而变成generator的函数，
+#  在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+# def odd():
+# 	print('1')
+# 	yield 1
+# 	print('2')
+# 	yield(2)
+# 	print('3')
+# 	yield(5)
+# o = odd()
+# next(o)
+# next(o)
+# next(o)
+
+
+#杨辉三角
+def triangles():
+	b = [1]
+	while True:
+		yield b
+		temp = b[:]
+		temp.append(0)
+		temp = [temp[x - 1] + temp[x] for x in range(len(temp))]
+		b = temp[:]
+
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    # print(results)
+    n = n + 1
+    if n == 10:
+        break
+
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
+	
 
 
 
