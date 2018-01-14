0f57f9a5df76abeca31957d5c074ecac127da3ee
jinweizhao <694951521@qq.com>
2018-01-14 22:34:22 +0800

    装饰器complete

--- learning.py
+++ learning.py
@@ -708,39 +708,116 @@
 # print(f.__name__)
 
 # decorator就是一个返回函数的高阶函数。所以，我们要定义一个能打印日志的decorator
-def log(func):
-	def wrapper(*args, **kw):
-		print('call %s():' % func.__name__)
-		return func(*args, **kw)
-	return wrapper
+# def log(func):
+# 	def wrapper(*args, **kw):
+# 		print('call %s():' % func.__name__)
+# 		return func(*args, **kw)
+# 	return wrapper
 
 # 观察上面的log，因为它是一个decorator，所以接受一个函数作为参数，并返回一个函数。
 # 	 我们要借助Python的@语法，把decorator置于函数的定义处：
 # @log
-def now():
-	print('2015-3-3')
+# def now():
+# 	print('2015-3-3')
 # 调用now()函数，不仅会运行now()函数本身，还会在运行now()函数前打印一行日志
 # now()
 # 把@log放到now()函数的定义处，相当于执行了语句
-abc = log(now)
-abc()
+# abc = log(now)
+# abc()
 # 如果decorator本身需要传入参数，那就需要编写一个返回decorator的高阶函数，
 #     写出来会更复杂。比如，要自定义log的文本
-def log(text):
-    def decorator(func):
-        def wrapper(*args, **kw):
-            print('%s %s():' % (text, func.__name__))
-            return func(*args, **kw)
-        return wrapper
-    return decorator
-@log('execute')
-def now():
-    print('2015-3-25')
+# def log(text):
+#     def decorator(func):
+#         def wrapper(*args, **kw):
+#             print('%s %s():' % (text, func.__name__))
+#             return func(*args, **kw)
+#         return wrapper
+#     return decorator
+# @log('execute')
+# def now():
+#     print('2015-3-25')
 
 # 和两层嵌套的decorator相比，3层嵌套的效果是这样的
-now = log('execute')(now)
+# now = log('execute')(now)
 # 我们来剖析上面的语句，首先执行log('execute')，返回的是decorator函数，
 # 再调用返回的函数，参数是now函数，返回值最终是wrapper函数
+# now()
+
+#以上两种decorator的定义都没有问题，但还差最后一步。因为我们讲了函数也是对象，
+#   它有__name__等属性，但你去看经过decorator装饰之后的函数，
+#   它们的__name__已经从原来的'now'变成了'wrapper'：
+#   不需要编写wrapper.__name__ = func.__name__这样的代码，
+#   Python内置的functools.wraps就是干这个事的，所以，一个完整的decorator的写法如下：
+
+# import functools
+# def log():
+# 	@functools.wraps(func)
+# 	def wrapper(*args, **kw):
+# 		print('call %s()' % func.__name__)
+# 		return func(*args, **kw)
+# 	return wrapper
+# 或者针对带参数的decorator：
+# import functools
+# def log(text):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(*args, **kw):
+#             print('%s %s():' % (text, func.__name__))
+#             return func(*args, **kw)
+#         return wrapper
+#     return decorator
+
+# import time, functools
+# def metric(fn):
+# 	@functools.wraps(fn)
+# 	def wrapper(*args, **kw):
+# 		begin = time.time()
+# 		result = fn(*args, **kw) # 函数在此执行
+# 		end = time.time()
+# 		print('%s executed in %s ms' % (fn.__name__, (end - begin)))
+# 		return result
+# 	return wrapper
+# @metric
+# def fast(x, y):
+#     time.sleep(0.0012)
+#     return x + y;
+# @metric
+# def slow(x, y, z):
+#     time.sleep(0.1234)
+#     return x * y * z;
+# f = fast(11, 22)
+# s = slow(11, 22, 33)
+# print(fast.__name__, s)
+# if f != 33:
+#     print('测试失败!')
+# elif s != 7986:
+#     print('测试失败!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
