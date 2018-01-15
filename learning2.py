26f2b6dad50434900ba99c23cfc825303e44633f
jinweizhao <694951521@qq.com>
2018-01-15 15:51:30 +0800

    begin 面向对象编程

---
+++ learning2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3  #可以让这个learning.py文件直接在Unix/Linux/Mac上运行
+# -*- coding: utf-8 -*-    #表示.py文件本身使用标准UTF-8编码
+
+
+# 面向对象编程
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
