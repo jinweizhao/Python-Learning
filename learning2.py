98b568e078d7b0cbd7f5f429885264b9dfd31a83
jinweizhao <694951521@qq.com>
2018-01-17 14:24:30 +0800

    面向对象编程 complete

--- learning2.py
+++ learning2.py
@@ -110,86 +110,214 @@ class Dog(Animal):
     def run(self):
         print('Dog is running...')
 
-class Cat(Animal):
+class Husky(Dog):
 
     def run(self):
-        print('Cat is running...')
+        print('Husky is running...')
 
-a = list() # a是list类型
-b = Animal() # b是Animal类型
-c = Dog() # c是Dog类型
+# class Cat(Animal):
+
+#     def run(self):
+#         print('Cat is running...')
+
+# a = list() # a是list类型
+# b = Animal() # b是Animal类型
+# c = Dog() # c是Dog类型
 
 #c不仅仅是Dog，c还是Animal
 # print(isinstance(c, Animal)) # True
 # print(isinstance(c, Dog)) # True
 
-def run_twice(ani):
-    	ani.run()
-    	ani.run()
+# def run_twice(ani):
+#     	ani.run()
+#     	ani.run()
 
-run_twice(b)
-run_twice(c)
+# run_twice(b)
+# run_twice(c)
 
-class Tortoise(Animal):
-    def run(self):
-        print('Tortoise is running slowly...')
+# class Tortoise(Animal):
+#     def run(self):
+#         print('Tortoise is running slowly...')
 
-run_twice(Tortoise())
+# run_twice(Tortoise())
 # 这就是著名的“开闭”原则：
 # 	对扩展开放：允许新增Animal子类；
 # 	对修改封闭：不需要修改依赖Animal类型的run_twice()等函数。
 
-class Timer(object):
-    def run(self):
-        print('Start...')
+# class Timer(object):
+#     def run(self):
+#         print('Start...')
 
-run_twice(Timer())
-
-
+# run_twice(Timer())
 
 
+# 获取对象信息
+# 来判断对象类型，使用type()函数
+# >>> type(abs)
+# <class 'builtin_function_or_method'>
+# >>> type(a)
+# <class '__main__.Animal'>
+# >>> type(abs)
+# <class 'builtin_function_or_method'>
+# >>> type(a)
+# <class '__main__.Animal'>
+
+# 如果要判断一个对象是否是函数怎么办？可以使用types模块中定义的常量：
+# import types
+# def fn():
+# 	pass
+# print(type(fn))   #<class 'function'>
+# >>> type(fn)==types.FunctionType
+# True
+# >>> type(abs)==types.BuiltinFunctionType
+# True
+# >>> type(lambda x: x)==types.LambdaType
+# True
+# >>> type((x for x in range(10)))==types.GeneratorType
+# True
+
+# 使用isinstance()
+# 对于class的继承关系来说，使用type()就很不方便。我们要判断class的类型，可以使用isinstance()函数
+# 继承关系是：object -> Animal -> Dog -> Husky
+a = Animal()
+d = Dog()
+h = Husky()
+
+# isinstance()判断的是一个对象是否是该类型本身，或者位于该类型的父继承链上。
+# >>> isinstance(h, Husky)
+# True
+# >>> isinstance(h, Dog)
+# True
 
 
+# 能用type()判断的基本类型也可以用isinstance()判断：
+# >>> isinstance('a', str)
+# True
+# >>> isinstance(123, int)
+# True
+# >>> isinstance(b'a', bytes)
+# True
+
+# 并且还可以判断一个变量是否是某些类型中的一种，比如下面的代码就可以判断是否是list或者tuple：
+# >>> isinstance([1, 2, 3], (list, tuple))
+# True
+# >>> isinstance((1, 2, 3), (list, tuple))
+# True
+
+# 总是优先使用isinstance()判断类型，可以将指定类型及其子类“一网打尽”。
 
 
+# dir():如果要获得一个对象的所有属性和方法，可以使用dir()函数，它返回一个包含字符串的list
+# print(dir('abc'))
+
+# 类似__xxx__的属性和方法在Python中都是有特殊用途的，比如__len__方法返回长度。在Python中，
+#     如果你调用len()函数试图获取一个对象的长度，实际上，在len()函数内部，
+#     它自动去调用该对象的__len__()方法，所以，下面的代码是等价的：
+# >>> len('ABC')
+# 3
+# >>> 'ABC'.__len__()
+# 3
+
+# 我们自己写的类，如果也想用len(myObj)的话，就自己写一个__len__()方法：
+# >>> class MyDog(object):
+# ...     def __len__(self):
+# ...         return 100
+# ...
+# >>> dog = MyDog()
+# >>> len(dog)
+# 100
 
 
+# 配合getattr()、setattr()以及hasattr()，我们可以直接操作一个对象的状态：
+# class MyObject(object):
+# 	def __init__(self):
+#         self.x = 9
+#     def power(self):
+#         return self.x * self.x
+# obj = MyObject()
+
+# >>> hasattr(obj, 'x') # 有属性'x'吗？
+# True
+# >>> obj.x
+# 9
+# >>> hasattr(obj, 'y') # 有属性'y'吗？
+# False
+# >>> setattr(obj, 'y', 19) # 设置一个属性'y'
+# >>> hasattr(obj, 'y') # 有属性'y'吗？
+# True
+# >>> getattr(obj, 'y') # 获取属性'y'
+# 19
+# >>> obj.y # 获取属性'y'
+# 19
+
+# 可以传入一个default参数，如果属性不存在，就返回默认值：
+# >>> getattr(obj, 'z', 404) # 获取属性'z'，如果不存在，返回默认值404
+# 404
+
+# 也可以获得对象的方法：
+# >>> hasattr(obj, 'power') # 有属性'power'吗？
+# True
+# >>> getattr(obj, 'power') # 获取属性'power'
+# <bound method MyObject.power of <__main__.MyObject object at 0x10077a6a0>>
+# >>> fn = getattr(obj, 'power') # 获取属性'power'并赋值到变量fn
+# >>> fn # fn指向obj.power
+# <bound method MyObject.power of <__main__.MyObject object at 0x10077a6a0>>
+# >>> fn() # 调用fn()与调用obj.power()是一样的
+# 81
+
+# python 中 or 和 and 的使用规则
+# 		x or y 的值只可能是x或y.  x为真就是x, x为假就是y
+# 		x or y 的值只可能是x或y.  x为真就是x, x为假就是y
+# 		对于, 1 or 5 and 4: 先算5 and 4, 5为真, 值为4. 再算1 or 4, 1 为真,值为1
+
+# 		对于, (1 or 5) and 4: 先算1 or 5, 1为真, 值为1. 再算1 and 4, 1为真,值为4
+
+# or的左右两边都为假时，返回最后一个假
+# and的左右两边都为真时，返回最后一个真
+
+# >>> tuple or list
+# <class 'tuple'>
+# >>> list or tuple
+# <class 'list'>
+# >>> tuple and list
+# <class 'list'>
+# >>> list and tuple
+# <class 'tuple'>
 
 
+# 实例属性和类属性
+# 由于Python是动态语言，根据类创建的实例可以任意绑定属性。给实例绑定属性的方法是通过实例变量，或者通过self变量：
+# class Student(object):
+#     def __init__(self, name):
+#         self.name = name
 
+# s = Student('Bob')
+# s.score = 90
+# print(s.score)   #90
+# print(hasattr(s, 'score'))   #True
 
+# 如果Student类本身需要绑定一个属性呢？可以直接在class中定义属性，
+# 这种属性是类属性，归Student类所有：
 
+# class Student(object):
+#     name = 'Student'
+# 当我们定义了一个类属性后，这个属性虽然归类所有，但类的所有实例都可以访问到。来测试一下：
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# >>> class Student(object):
+# ...     name = 'Student'
+# >>> s = Student() # 创建实例s
+# >>> print(s.name) # 打印name属性，因为实例并没有name属性，所以会继续查找class的name属性
+# Student
+# >>> print(Student.name) # 打印类的name属性
+# Student
+# >>> s.name = 'Michael' # 给实例绑定name属性
+# >>> print(s.name) # 由于实例属性优先级比类属性高，因此，它会屏蔽掉类的name属性
+# Michael
+# >>> print(Student.name) # 但是类属性并未消失，用Student.name仍然可以访问
+# Student
+# >>> del s.name # 如果删除实例的name属性
+# >>> print(s.name) # 再次调用s.name，由于实例的name属性没有找到，类的name属性就显示出来了
+# Student
 
 
 
