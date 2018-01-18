300138d8579baf2efeabb234c06b9160a7412a1a
jinweizhao <694951521@qq.com>
2018-01-18 15:23:00 +0800

    面向对象高级编程complete

--- learning2.py
+++ learning2.py
@@ -441,16 +441,259 @@ h = Husky()
 # 3.如果对下一个类存在两个合法的选择，选择第一个父类
 
 
-# 定制类
+
+# 定制类 例如 __slots__
+
+# __str__
+# >>> class Student(object):
+# ...     def __init__(self, name):
+# ...         self.name = name
+# ...
+# >>> print(Student('Michael'))
+# <__main__.Student object at 0x109afb190>
+# 打印出一堆<__main__.Student object at 0x109afb190>，不好看
+
+# >>> class Student(object):
+# ...     def __init__(self, name):
+# ...         self.name = name
+# ...     def __str__(self):
+# ...         return 'Student object (name: %s)' % self.name
+# ...
+# >>> print(Student('Michael'))
+# Student object (name: Michael)
+
+# >>> s = Student('Michael')
+# >>> s
+# <__main__.Student object at 0x109afb310>
+# 这是因为直接显示变量调用的不是__str__()，而是__repr__()，两者的区别是__str__()返回用户看到的字符串，
+#     而__repr__()返回程序开发者看到的字符串，也就是说，__repr__()是为调试服务的。
+
+# 解决办法是再定义一个__repr__()。但是通常__str__()和__repr__()代码都是一样的，所以，有个偷懒的写法：
+
+# class Student(object):
+#     def __init__(self, name):
+#         self.name = name
+#     def __str__(self):
+#         return 'Student object (name=%s)' % self.name
+#     __repr__ = __str__
 
 
+# __iter__
+# 	如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，
+# 		该方法返回一个迭代对象，然后，Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，
+# 		直到遇到StopIteration错误时退出循环。
+
+# 以斐波那契数列为例
+# class Fib(object):
+# 	def __init__(self):
+# 		self.a, self.b = 0, 1
+
+# 	def __iter__(self):
+# 		return self  # 实例本身就是迭代对象，故返回自己
+# 	def __next__(self):
+# 		self.a, self.b = self.b, self.a + self.b   # 计算下一个值
+# 		if self.a > 100000:  # 退出循环的条件
+# 			raise StopIteration()
+# 		return self.a  # 返回下一个值
+# for n in Fib():
+# 	print(n)
 
 
+# __getitem__ 
+# 	Fib实例虽然能作用于for循环，看起来和list有点像，
+# 	但是，把它当成list来使用还是不行，比如，取第5个元素：
+# >>> Fib()[5]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'Fib' object does not support indexing
+
+# 要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+# class Fib(object):
+#     def __getitem__(self, n):
+#         a, b = 1, 1
+#         for x in range(n):
+#             a, b = b, a + b
+#         return a
+# >>> f = Fib()
+# >>> f[0]
+# 1
+# >>> f[1]
+# 1
+# >>> f[2]
+
+# 这样不支持切片方法：原因是__getitem__()传入的参数可能是一个int，
+# 		也可能是一个切片对象slice，所以要做判断：
+# class Fib(object):
+#     def __getitem__(self, n):
+#         if isinstance(n, int): # n是索引
+#             a, b = 1, 1
+#             for x in range(n):
+#                 a, b = b, a + b
+#             return a
+#         if isinstance(n, slice): # n是切片
+#             start = n.start
+#             stop = n.stop
+#             if start is None:
+#                 start = 0
+#             a, b = 1, 1
+#             L = []
+#             for x in range(stop):
+#                 if x >= start:
+#                     L.append(a)
+#                 a, b = b, a + b
+#             return L
 
 
+# __getattr__   
+# 调用一个类中不存在的属性时会报错
+# 要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，
+#     那就是写一个__getattr__()方法，动态返回一个属性。修改如下
+# class Student(object):
+#     def __init__(self):
+#         self.name = 'Michael'
+
+#     def __getattr__(self, attr):
+#         if attr=='score':
+#             return 99
+# 当调用不存在的属性时，比如score，Python解释器会试图调用__getattr__(self, 'score')来尝试获得属性
+# 只有在没有找到属性的情况下，才调用__getattr__，已有的属性，比如name，不会在__getattr__中查找。
+
+# 注意到任意调用如s.abc都会返回None，这是因为我们定义的__getattr__默认返回就是None。
+# 		要让class只响应特定的几个属性，我们就要按照约定，抛出AttributeError的错误：
+# class Student(object):
+
+#     def __getattr__(self, attr):
+#         if attr=='age':
+#             return lambda: 25
+#         raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+# REST API  /  链式调用   略
 
 
+#  __call__
+# 当我们调用实例方法时，我们用instance.method()来调用。实现直接在实例本身上调用
+# class Student(object):
+#     def __init__(self, name):
+#         self.name = name
 
+#     def __call__(self):
+#         print('My name is %s.' % self.name)
+# >>> s = Student('Michael')
+# >>> s() # self参数不要传入
+# My name is Michael.
+
+# 怎么判断一个变量是对象还是函数呢？其实，更多的时候，我们需要判断一个对象是否能被调用，
+# 能被调用的对象就是一个Callable对象，比如函数和我们上面定义的带有__call__()的类实例：
+# >>> callable(Student())
+# True
+# >>> callable(max)
+# True
+# >>> callable([1, 2, 3])
+# False
+# >>> callable(None)
+# False
+# >>> callable('str')
+# False
+# 通过callable()函数，我们就可以判断一个对象是否是“可调用”对象。
+
+# 练习
+# class Chain(object):
+#     def __init__(self,path=''):
+#         self._path=path
+#     def __str__(self):
+#         return self._path
+#     def __getattr__(self,path):
+#         return Chain('%s/%s' % (self._path,path))
+#     __call__=__getattr__
+#     __repr__=__str__
+
+# print(Chain().status.user.timeline.list)
+# print(Chain().users('Zhangsan').repos)
+
+
+#  使用枚举类
+from enum import Enum
+Month = Enum('Month', 
+	('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+# 这样我们就获得了Month类型的枚举类，可以直接使用Month.Jan来引用一个常量，或者枚举它的所有成员：
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+# value属性则是自动赋给成员的int常量，默认从1开始计数。
+
+# 如果需要更精确地控制枚举类型，可以从Enum派生出自定义类：
+from enum import Enum, unique
+
+@unique
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+# @unique装饰器可以帮助我们检查保证没有重复值。
+
+# 使用：
+# >>> day1 = Weekday.Mon
+# >>> print(day1)
+# Weekday.Mon
+# >>> print(Weekday.Tue)
+# Weekday.Tue
+# >>> print(Weekday['Tue'])
+# Weekday.Tue
+# >>> print(Weekday.Tue.value)
+# 2
+# >>> print(day1 == Weekday.Mon)
+# True
+# >>> print(day1 == Weekday.Tue)
+# False
+# >>> print(Weekday(1))
+# Weekday.Mon
+# >>> print(day1 == Weekday(1))
+# True
+
+
+#   使用元类
+# >>> def fn(self, name='world'): # 先定义函数
+# ...     print('Hello, %s.' % name)
+# ...
+# >>> Hello = type('Hello', (object,), dict(hello=fn)) # 创建Hello class
+# >>> h = Hello()
+# >>> h.hello()
+# Hello, world.
+# >>> print(type(Hello))
+# <class 'type'>
+# >>> print(type(h))
+# <class '__main__.Hello'>
+
+# 要创建一个class对象，type()函数依次传入3个参数：
+
+# 1，class的名称；
+# 2，继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+# 3，class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+
+# metaclass : 定义metaclass，就可以创建类，最后创建实例。
+# 我们先看一个简单的例子，这个metaclass可以给我们自定义的MyList增加一个add方法：
+
+# 定义ListMetaclass，按照默认习惯，metaclass的类名总是以Metaclass结尾，以便清楚地表示这是一个metaclass：
+
+# # metaclass是类的模板，所以必须从`type`类型派生：
+# class ListMetaclass(type):
+#     def __new__(cls, name, bases, attrs):
+#         attrs['add'] = lambda self, value: self.append(value)
+#         return type.__new__(cls, name, bases, attrs)
+# 有了ListMetaclass，我们在定义类的时候还要指示使用ListMetaclass来定制类，传入关键字参数metaclass：
+
+# class MyList(list, metaclass=ListMetaclass):
+#     pass
+# 当我们传入关键字参数metaclass时，魔术就生效了，它指示Python解释器在创建MyList时，要通过ListMetaclass.__new__()来创建，在此，我们可以修改类的定义，比如，加上新的方法，然后，返回修改后的定义。
+
+# __new__()方法接收到的参数依次是：
+# 1,当前准备创建的类的对象；
+# 2,类的名字；
+# 3,类继承的父类集合；
+# 4,类的方法集合。
 
 
 
