5e7f1f1886d5283c5b34b1db19ef894b0981768a
jinweizhao <694951521@qq.com>
2018-02-08 17:39:03 +0800

    调试 complete

--- learning3.py
+++ learning3.py
@@ -81,27 +81,97 @@
 # 比如ValueError，TypeError），尽量使用Python内置的错误类型。
 
 # eg
-def foo(s):
-    n = int(s)
-    if n==0:
-        raise ValueError('invalid value: %s' % s)
-    return 10 / n
+# def foo(s):
+#     n = int(s)
+#     if n==0:
+#         raise ValueError('invalid value: %s' % s)
+#     return 10 / n
 
-def bar():
-    try:
-        foo('0')
-    except ValueError as e:
-        print('ValueError!')
-        raise   #raise语句如果不带参数，就会把当前错误原样抛出。
+# def bar():
+#     try:
+#         foo('0')
+#     except ValueError as e:
+#         print('ValueError!')
+#         raise   #raise语句如果不带参数，就会把当前错误原样抛出。
 
-bar()
+# bar()
 # raise语句如果不带参数，就会把当前错误原样抛出。
 # 此外，在except中raise一个Error，还可以把一种类型的错误转化成另一种类型：
 
 
+#  调试  断言（assert）
+# def foo(s):
+#     n = int(s)
+#     assert n != 0, 'n is zero!'
+#     return 10 / n
+
+# def main():
+#     foo('0')
+# assert的意思是，表达式n != 0应该是True，否则，根据程序运行的逻辑，后面的代码肯定会出错。
+# 程序中如果到处充斥着assert，和print()相比也好不到哪去。
+# 不过，启动Python解释器时可以用-O参数来关闭assert  $ python -O err.py
+# 关闭后，你可以把所有的assert语句当成pass来看
 
 
+# logging ：和assert比，logging不会抛出错误，而且可以输出到文件
+# import logging
 
+# s = '0'
+# n = int(s)
+# logging.info('n = %d' % n)
+# print(10 / n)
+# logging.info()就可以输出一段文本。运行，发现除了ZeroDivisionError，没有任何信息。怎么回事？
+
+# logging之后添加一行配置再试试：
+
+# import logging
+# logging.basicConfig(level=logging.INFO)
+# 看到输出了：
+
+# $ python err.py
+# INFO:root:n = 0
+# Traceback (most recent call last):
+#   File "err.py", line 8, in <module>
+#     print(10 / n)
+# ZeroDivisionError: division by zero
+# logging的好处，它允许你指定记录信息的级别，有debug，info，warning，error等几个级别，
+# 当我们指定level=INFO时，logging.debug就不起作用了。同理，指定level=WARNING后，
+# debug和info就不起作用了。这样一来，你可以放心地输出不同级别的信息，也不用删除，最后统一控制输出哪个级别的信息。
+
+# logging的另一个好处是通过简单的配置，一条语句可以同时输出到不同的地方，比如console和文件。
+
+
+# pdb : 启动Python的调试器pdb，让程序以单步方式运行
+# 我们先准备好程序：
+# s = '0'
+# n = int(s)
+# print(10 / n)
+# 然后启动：
+# $ python -m pdb learning3.py
+# 以参数-m pdb启动后，pdb定位到下一步要执行的代码-> s = '0'。输入命令l来查看代码：
+# 输入命令 n 可以单步执行代码：
+# 任何时候都可以输入命令 p 变量名来查看变量：
+# 输入命令 q 结束调试，退出程序：
+# 如果有一千行代码，要运行到第999行得敲多少命令啊。还好，我们还有另一种调试方法。
+
+# pdb.set_trace()
+# 这个方法也是用pdb，但是不需要单步执行，我们只需要import pdb，
+# 然后，在可能出错的地方放一个pdb.set_trace()，就可以设置一个断点：
+import pdb
+
+s = '0'
+n = int(s)
+pdb.set_trace() # 运行到这里会自动暂停
+print(10 / n)
+# 运行代码，程序会自动在pdb.set_trace()暂停并进入pdb调试环境，
+# 可以用命令 p 查看变量，或者用命令 c 继续运行
+
+# IDE 
+# 如果要比较爽地设置断点、单步执行，就需要一个支持调试功能的IDE。目前比较好的Python IDE有：
+
+# Visual Studio Code：https://code.visualstudio.com/，需要安装Python插件。
+
+# PyCharm：http://www.jetbrains.com/pycharm/
 
 
 
