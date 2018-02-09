4e44553dd8f01bab335761a0ae5f8b36d7071f0f
jinweizhao <694951521@qq.com>
2018-02-09 17:44:00 +0800

    单元测试 begin

--- learning3.py
+++ learning3.py
@@ -157,23 +157,21 @@
 # pdb.set_trace()
 # 这个方法也是用pdb，但是不需要单步执行，我们只需要import pdb，
 # 然后，在可能出错的地方放一个pdb.set_trace()，就可以设置一个断点：
-import pdb
+# import pdb
 
-s = '0'
-n = int(s)
-pdb.set_trace() # 运行到这里会自动暂停
-print(10 / n)
+# s = '0'
+# n = int(s)
+# pdb.set_trace() # 运行到这里会自动暂停
+# print(10 / n)
 # 运行代码，程序会自动在pdb.set_trace()暂停并进入pdb调试环境，
 # 可以用命令 p 查看变量，或者用命令 c 继续运行
 
 # IDE 
 # 如果要比较爽地设置断点、单步执行，就需要一个支持调试功能的IDE。目前比较好的Python IDE有：
-
 # Visual Studio Code：https://code.visualstudio.com/，需要安装Python插件。
-
 # PyCharm：http://www.jetbrains.com/pycharm/
 
-
+#  单元测试
 
 
 
