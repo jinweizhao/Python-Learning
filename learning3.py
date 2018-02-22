345c50668a3a987a13890ecb79ac177d5f249117
jinweizhao <694951521@qq.com>
2018-02-22 17:49:39 +0800

    1

--- learning3.py
+++ learning3.py
@@ -173,12 +173,38 @@
 
 #  单元测试
 
+# 我们来编写一个Dict类，这个类的行为和dict一致，但是可以通过属性来访问
+class Dict(dict):
+
+	def __init__(self, **kw):
+		super().__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except Exception as e:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+# d = Dict(a=1, b=2)
+
+# print(d['a'])
+# print(d.a)
 
 
+# 为了编写单元测试，我们需要引入Python自带的unittest模块
+import unittest
 
+class TestDict(unittest.TestCase):
 
-
-
+	def test_init(self):
+		d = Dict(a = 1, b = 'test')
+		self.assertEqual(d.a, 1)
+		self.assertEqual()
+		self.assertTrue(isinstance(d, dict))
+	
 
 
 
